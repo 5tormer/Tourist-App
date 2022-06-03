171713efb3b95775deb50b104257523d621ebc51
5tormer <4509234@gmail.com>
2022-06-03 16:47:04 +0300

    Changed map locale

--- android/app/src/main/kotlin/com/example/tourist_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tourist_app/MainActivity.kt
@@ -8,6 +8,7 @@ import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
   override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setLocale("ru_RU")
     MapKitFactory.setApiKey("0fa596f8-2e02-4d03-bf95-941340a1c1ef")
     GeneratedPluginRegistrant.registerWith(flutterEngine)
   }
