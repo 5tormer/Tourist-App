55476d901d12434232e197ce2d67fcb8a41e27d9
5tormer <4509234@gmail.com>
2022-06-03 16:40:59 +0300

    Added Yandex MapKit as a dependency

--- android/app/src/main/kotlin/com/example/tourist_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/tourist_app/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.tourist_app
 
+import androidx.annotation.NonNull
+import com.yandex.mapkit.MapKitFactory
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setApiKey("0fa596f8-2e02-4d03-bf95-941340a1c1ef")
+    GeneratedPluginRegistrant.registerWith(flutterEngine)
+  }
 }
