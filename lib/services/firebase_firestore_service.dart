5c591185e10336029dbe28e1e180efe92047b537
5tormer <4509234@gmail.com>
2022-06-04 14:16:23 +0300

    Added firebase firestore service

---
+++ lib/services/firebase_firestore_service.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseFirestoreService {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  Future<void> setDataForCollection(
+      {required String collectionName,
+      required String userEmail,
+      required String text}) async {
+    await _db.collection(collectionName).doc(userEmail).set(
+      {'text': text},
+    );
+  }
+
+  Future<void> setArrayDataForCollection(
+      {required String collectionName,
+      required String userEmail,
+      required List<String> list}) async {
+    await _db.collection(collectionName).doc(userEmail).set(
+      {'text': list},
+    );
+  }
+
+  Future<List<String>?> getDataFromCollection(
+      {required String userEmail,
+      required String collectionName,
+      required String key}) async {
+    List<String> list = [];
+    QuerySnapshot<Map<String, dynamic>> data = await _db
+        .collection(collectionName)
+        .where('__name__', isEqualTo: userEmail)
+        .get();
+    if (data.docs.isNotEmpty) {
+      for (var doc in data.docs) {
+        list.add(doc.data()[key]);
+      }
+      return list;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List<String>?> getArrayDataFromCollection(
+      {required String userEmail,
+      required String collectionName,
+      required String key}) async {
+    List<String> list = [];
+    QuerySnapshot<Map<String, dynamic>> data = await _db
+        .collection(collectionName)
+        .where('__name__', isEqualTo: userEmail)
+        .get();
+    if (data.docs.isNotEmpty) {
+      for (var doc in data.docs) {
+        List<String> localList = (doc.data()[key] as List<dynamic>)
+            .map((e) => e.toString())
+            .toList();
+        list = localList;
+      }
+      return list;
+    } else {
+      return null;
+    }
+  }
+}
